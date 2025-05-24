d15ed53fcc97690d1ec03a3a227d367298dc3903
Jose1-0 <josealedrr@gmail.com>
2025-05-24 00:48:39 -0300

    criacao da exceptionde transação, criacao do serviço de pix

---
+++ src/main/java/com/desafio/ngbilling/exception/TransacaoException.java
@@ -0,0 +1,9 @@
+package com.desafio.ngbilling.exception;
+
+public class TransacaoException extends RuntimeException {
+	
+	public TransacaoException(String mensagem) {
+		super(mensagem);
+	}
+
+}
---
+++ src/main/java/com/desafio/ngbilling/service/impl/PixTransacaoService.java
@@ -0,0 +1,13 @@
+package com.desafio.ngbilling.service.impl;
+
+import com.desafio.ngbilling.model.Conta;
+import com.desafio.ngbilling.service.Itransacao;
+
+public class PixTransacaoService implements Itransacao{
+
+	@Override
+	public void transacao(Conta conta, float valor) {
+		
+	}
+
+}
